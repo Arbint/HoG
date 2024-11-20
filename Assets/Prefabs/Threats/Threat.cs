127d1ff5966840ab20700ca7dd462b5fd692c951
Li <jili1@uiwtx.edu>
2024-11-20 16:09:49 -0600

    added the threat class

---
+++ Assets/Prefabs/Threats/Threat.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(HealthComponent))]
+public abstract class Threat : MonoBehaviour, ITeamInterface, IGrabbable
+{
+    [SerializeField] int teamId = 1;
+    [SerializeField] Transform deathVFXTransform;
+    [SerializeField] GameObject deadVFXPrefab;
+    [SerializeField] float deadVFXScale = 1;
+    HealthComponent _healthComponent;
+
+    private void Awake()
+    {
+        _healthComponent = GetComponent<HealthComponent>();
+        _healthComponent.OnDead += Blowup;
+    }
+
+    protected virtual void Blowup(GameObject killer)
+    {
+        GameObject deadVFX = Instantiate(deadVFXPrefab, deathVFXTransform.position, deathVFXTransform.rotation);
+        deadVFX.transform.localScale = Vector3.one * deadVFXScale;
+        Destroy(gameObject);
+    }
+
+    public int GetTeamID()
+    {
+        return teamId;
+    }
+
+    public abstract void GrabbedBy(GameObject grabber, Vector3 grabPoint);
+
+    public abstract void Released();
+} 
