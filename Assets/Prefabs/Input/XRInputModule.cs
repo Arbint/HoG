7b1d1776ecea53bae98a3ab79eb469c7556062b9
Li <jili1@uiwtx.edu>
2024-12-02 16:37:19 -0600

    added button down

--- Assets/Prefabs/Input/XRInputModule.cs
+++ Assets/Prefabs/Input/XRInputModule.cs
@@ -1,3 +1,6 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -62,11 +65,58 @@ public class XRInputModule : PointerInputModule
 
         _leftPointerEventData = new PointerEventData(eventSystem);
         _rightPointerEventData = new PointerEventData(eventSystem);
+
+        BindInput();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void BindInput()
+    {
+        if(_playerInput != null)
+        {
+            _playerInput.LeftXRController.Position.performed += (context) => ProcessPointerMove(_leftPointerInterface, _leftPointerEventData);
+            _playerInput.LeftXRController.Trigger.performed += (context) => ProcessPointerDown(_leftPointerInterface, _leftPointerEventData);
+            _playerInput.LeftXRController.Trigger.canceled += (context) => ProcessPointerUp(_leftPointerInterface, _leftPointerEventData);
+
+            _playerInput.RightXRController.Position.performed += (context) => ProcessPointerMove(_rightPointerInterface, _rightPointerEventData);
+            _playerInput.RightXRController.Trigger.performed += (context) => ProcessPointerDown(_rightPointerInterface, _rightPointerEventData);
+            _playerInput.RightXRController.Trigger.canceled += (context) => ProcessPointerUp(_rightPointerInterface, _rightPointerEventData);
+        }
+    }
+
+    private void ProcessPointerUp(IXRPointer pointerInterface, PointerEventData pointerEventData)
     {
         
+    }
+
+    private void ProcessPointerDown(IXRPointer pointerInterface, PointerEventData pointerEventData)
+    {
+        if (pointerInterface == null || pointerEventData == null)
+            return;
+
+        Vector2 pointerScreenPos = pointerInterface.GetPointerScreenPosition();
+        pointerEventData.position = pointerScreenPos;
+        pointerEventData.pointerCurrentRaycast = GetRaycastResultFromEventData(pointerEventData);
+
+        GameObject pointerDownGameObject = ExecuteEvents.GetEventHandler<IPointerDownHandler>(pointerEventData.pointerCurrentRaycast.gameObject); 
+        if(pointerDownGameObject)
+        {
+            pointerEventData.pointerPress = pointerDownGameObject;
+            pointerEventData.pointerPressRaycast = pointerEventData.pointerCurrentRaycast;
+            pointerEventData.eligibleForClick = true;
+
+            ExecuteEvents.Execute(pointerDownGameObject, pointerEventData, ExecuteEvents.pointerDownHandler);
+        }
+    }
+
+    private void ProcessPointerMove(IXRPointer pointerInterface, PointerEventData pointerEventData)
+    {
+
+    }
+
+    RaycastResult GetRaycastResultFromEventData(PointerEventData pointerEventData)
+    {
+        List<RaycastResult> raycastResults = new List<RaycastResult>();
+        eventSystem.RaycastAll(pointerEventData, raycastResults);
+        return FindFirstRaycast(raycastResults);
     }
 }
